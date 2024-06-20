5063382bb8965d62069c315b27efe16868fa1682
Hknots <hknots99@gmail.com>
2024-06-20 10:46:45 +0200

    add ElevKafkaConsumer

---
+++ src/main/java/no/fintlabs/elev/Elev.java
@@ -0,0 +1,15 @@
+package no.fintlabs.elev;
+
+import lombok.Data;
+
+@Data
+public class Elev {
+
+    private String id;
+    private String name;
+    private String birthNumber;
+    private String email;
+    private String mobileNumber;
+    private String averageGrades;
+
+}
